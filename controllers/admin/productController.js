8a535f1b7ad46fc2838bc077c80f80b96b653285
risansadik <risansadik@gmail.com>
2025-01-10 23:42:29 +0530

    Update productController.js

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../../models/productSchema');
 const Category = require('../../models/categorySchema');
-const Brand = require('../../models/brandSchema');
+
 const User = require('../../models/userSchema');
 const fs = require('fs');
 const path = require('path');
